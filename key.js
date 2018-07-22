d124a6178e47c537a07fad60aa0c3cc0f9fabef8
Sam Ilkov <samuil.ilkov90@gmail.com>
2018-07-22 18:59:52 -0400

    twitter and spotify keys issue

--- key.js
+++ key.js
@@ -1,10 +1,10 @@
 console.log('this is loaded');
 
 exports.twitter = {
-  consumer_key: process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  consumer_key: QOLwe1RcfQkn6DZgVzmUwyhsd,
+  consumer_secret: YmatkejNfb6psEo7OZJuZhP87O77CP4eJKtJ7134LqIlNbcL2j,
+  access_token_key: QGYv1sKaIGoQJy7IOYRCPeQP4AGYOo,
+  access_token_secret: r8CLVRbFAV20bmv7rCom3H9XMLvZJ7IQmMdpFojEM1Lbf
 };
 
 exports.spotify = {
