59c3491aba1d29b2aa2d656cf3b8cffaf63df09a
Sam Ilkov <samuil.ilkov90@gmail.com>
2018-07-22 12:11:59 -0400

    changes

--- key.js
+++ key.js
@@ -1,15 +1,13 @@
 console.log('this is loaded');
 
 exports.twitter = {
-  consumer_key: process.env.QOLwe1RcfQkn6DZgVzmUwyhsd,
-  consumer_secret: process.env.YmatkejNfb6psEo7OZJuZhP87O77CP4eJKtJ7134LqIlNbcL2j,
-  access_token_key: process.env.QGYv1sKaIGoQJy7IOYRCPeQP4AGYOo,
-  access_token_secret: process.env.r8CLVRbFAV20bmv7rCom3H9XMLvZJ7IQmMdpFojEM1Lbf
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 };
 
 exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
-
-
